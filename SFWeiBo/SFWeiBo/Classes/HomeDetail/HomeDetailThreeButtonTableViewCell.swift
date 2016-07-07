021e649a46407bc2de6f12e42f7667021942416a
1170197998 <1170197998@qq.com>
2016-07-07 18:40:18 +0800

    add

---
+++ SFWeiBo/SFWeiBo/Classes/HomeDetail/HomeDetailThreeButtonTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeDetailThreeButtonTableViewCell.swift
+//  SFWeiBo
+//
+//  Created by ShaoFeng on 16/7/7.
+//  Copyright © 2016年 ShaoFeng. All rights reserved.
+//
+
+import UIKit
+
+class HomeDetailThreeButtonTableViewCell: UITableViewCell {
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
