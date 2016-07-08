77f0ceffe619790409992f03c17120d8859911b0
1170197998 <1170197998@qq.com>
2016-07-08 18:23:07 +0800

    add

--- SFWeiBo/SFWeiBo/Classes/HomeDetail/HomeDetailTableViewController.swift
+++ SFWeiBo/SFWeiBo/Classes/HomeDetail/HomeDetailTableViewController.swift
@@ -36,7 +36,7 @@ class HomeDetailViewController: UIViewController {
     //MARK: - 懒加载
     private lazy var footerButton: StatusTableViewBottomView = StatusTableViewBottomView()
     private lazy var tableView: UITableView = {
-        let tableView = UITableView()
+        let tableView = UITableView.init(frame: CGRectZero, style: UITableViewStyle.Grouped)
         tableView.dataSource = self
         tableView.delegate = self
         return tableView
@@ -50,8 +50,35 @@ class HomeDetailViewController: UIViewController {
 
 extension HomeDetailViewController: UITableViewDelegate,UITableViewDataSource {
     
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        
+        if section == 1 {
+            return 44
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0.000000000001
+    }
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        if section == 1 {
+            let threeButton = StatusTableViewBottomView()
+            return threeButton
+        } else {
+            return nil
+        }
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
@@ -72,12 +99,23 @@ extension HomeDetailViewController: UITableViewDelegate,UITableViewDataSource {
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        var cell = tableView.dequeueReusableCellWithIdentifier(StatusTableViewCellIdentifier.cellID(currentStatus!)) as? StatusTableViewCell
-        if (cell == nil) {
-            cell = StatusTableViewCell.init(style: UITableViewCellStyle.Default, reuseIdentifier: StatusTableViewCellIdentifier.cellID(currentStatus!))
+        if indexPath.section == 0 {
+            
+            var cell = tableView.dequeueReusableCellWithIdentifier(StatusTableViewCellIdentifier.cellID(currentStatus!)) as? StatusTableViewCell
+            if (cell == nil) {
+                cell = StatusTableViewCell.init(style: UITableViewCellStyle.Default, reuseIdentifier: StatusTableViewCellIdentifier.cellID(currentStatus!))
+            }
+            cell?.status = currentStatus
+            cell?.footerView.hidden = true
+            return cell!
+            
+        } else {
+            
+            var cell = tableView.dequeueReusableCellWithIdentifier("cellID")
+            if (cell == nil) {
+                cell = UITableViewCell.init(style: UITableViewCellStyle.Default, reuseIdentifier: "cellID")
+            }
+            return cell!
         }
-        cell?.status = currentStatus
-        cell?.footerView.hidden = true
-        return cell!
     }
 }
--- SFWeiBo/SFWeiBo/Classes/HomeDetail/HomeDetailThreeButtonTableViewCell.swift
+++ SFWeiBo/SFWeiBo/Classes/HomeDetail/HomeDetailThreeButtonTableViewCell.swift
@@ -5,19 +5,40 @@
 //  Created by ShaoFeng on 16/7/7.
 //  Copyright © 2016年 ShaoFeng. All rights reserved.
 //
+/*
+ override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+ super.init(style: style, reuseIdentifier: reuseIdentifier)
+ setupUI()
+ }
+ 
+ private func setupUI() {
+ contentView.addSubview(threeButton)
+ threeButton.AlignVertical(type: AlignType.TopLeft, referView: contentView, size: CGSizeMake(UIScreen.mainScreen().bounds.width, 44), offset: CGPointMake(0, 0))
+ }
+ 
+ private lazy var threeButton: StatusTableViewBottomView = StatusTableViewBottomView()
+ */
 
 import UIKit
 
 class HomeDetailThreeButtonTableViewCell: UITableViewCell {
 
+    var mArray: [String] = []
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        mArray = Array()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        mArray = ["评论","赞","转发"]
         
     }
+    
     
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
 }
